864de2673dcdfaf0fa30c0a1e774fbb348155897
Castorice2071 <elvmx47@gmail.com>
2025-07-15 09:11:29 +0800

    1

--- test/test.jsx
+++ test/test.jsx
@@ -48,13 +48,125 @@ function getSelectedColors(the_obj) {
     return result;
 }
 
+/**
+ * 获取文档中所有的颜色
+ */
+function getAllColors() {
+    var doc = app.activeDocument;
+    var colors = [];
+
+    function processLayer(layerOrGroupItem) {
+        // 如果图层被锁定或隐藏，跳过
+        if (layerOrGroupItem.locked || layerOrGroupItem.hidden) return;
+
+        // 遍历 layerOrGroupItem 的子元素
+        for (var i = 0; i < layerOrGroupItem.pageItems.length; i++) {
+            var item = layerOrGroupItem.pageItems[i];
+
+            // 如果当前对象是 Group 或者 Layer，继续递归
+            if (item.typename === "GroupItem" || item.typename === "Layer") {
+                processLayer(item);
+            } else {
+                collectColors(item);
+            }
+        }
+    }
+
+    function collectColors(item) {
+        $.writeln("collectColors item.typename: " + item.typename);
+        if (item.typename === "GroupItem") {
+            for (var i = 0; i < item.pageItems.length; i++) {
+                collectColors(item.pageItems[i]);
+            }
+        } else if (item.typename === "CompoundPathItem") {
+            // 处理复合路径中的每个路径项
+            for (var i = 0; i < item.pathItems.length; i++) {
+                var pathItem = item.pathItems[i];
+                if (pathItem.filled && pathItem.fillColor) {
+                    addColor(pathItem.fillColor);
+                }
+            }
+        } else {
+            if (item.filled && item.fillColor) {
+                addColor(item.fillColor);
+            }
+        }
+    }
+
+    function addColor(color) {
+        if (color.typename === "RGBColor") {
+            colors.push("RGB(" + color.red + ", " + color.green + ", " + color.blue + ")");
+        } else if (color.typename === "CMYKColor") {
+            colors.push("CMYK(" + color.cyan + ", " + color.magenta + ", " + color.yellow + ", " + color.black + ")");
+        } else if (color.typename === "GrayColor") {
+            colors.push("Gray(" + color.gray + ")");
+        } else if (color.typename === "SpotColor") {
+            colors.push("SpotColor(" + color.spot.name + ")");
+        }
+    }
+
+    // 检查文档是否存在且有图层
+    if (doc && doc.layers.length > 0) {
+        // 遍历所有图层
+        for (var i = 0; i < doc.layers.length; i++) {
+            processLayer(doc.layers[i]);
+        }
+    }
+
+    // 去重处理
+    var uniqueColors = {};
+    var result = [];
+    for (var j = 0; j < colors.length; j++) {
+        var c = colors[j];
+        if (!uniqueColors[c]) {
+            uniqueColors[c] = true;
+            result.push(c);
+        }
+    }
+    return result;
+}
+
+/**
+ * 隐藏非此颜色的项
+ */
+function hiddenPathByColor(colorStr) {
+    // 获取当前文档
+    var doc = app.activeDocument;
+
+    // 遍历所有路径项并隐藏不匹配当前颜色的项
+    for (var j = 0; j < doc.pathItems.length; j++) {
+        var pathItem = doc.pathItems[j];
+        if (pathItem.filled && pathItem.fillColor) {
+            var itemColorStr = "";
+            var color = pathItem.fillColor;
+
+            if (color.typename === "RGBColor") {
+                itemColorStr = "RGB(" + color.red + ", " + color.green + ", " + color.blue + ")";
+            } else if (color.typename === "CMYKColor") {
+                itemColorStr = "CMYK(" + color.cyan + ", " + color.magenta + ", " + color.yellow + ", " + color.black + ")";
+            } else if (color.typename === "GrayColor") {
+                itemColorStr = "Gray(" + color.gray + ")";
+            } else if (color.typename === "SpotColor") {
+                itemColorStr = "SpotColor(" + color.spot.name + ")";
+            }
+
+            pathItem.hidden = itemColorStr !== colorStr;
+        }
+    }
+}
+
 var doc = app.activeDocument;
 
-var item = doc.pageItems.getByName("zzz");
+var item = doc.pageItems.getByName("我的天");
 
-var colors = getSelectedColors(item);
+// var colors = getSelectedColors(item);
+// var colors = getAllColors();
 
-$.writeln("item: " + item);
-$.writeln("item.pathItems: " + item.pathItems);
-$.writeln("colors.length: " + colors.length);
-$.writeln("colors: " + colors);
+// $.writeln("item: " + item);
+// $.writeln("item.pathItems: " + item.pathItems);
+// $.writeln("colors.length: " + colors.length);
+// $.writeln("colors: " + colors);
+
+hiddenPathByColor("RGB(253, 233, 156)")
+
+// app.undo();
--- test/test2.jsx
+++ test/test2.jsx
@@ -1,8 +1,74 @@
-//@target illustrator
-//@targetengine main
+function analyzeColorsAndExportPSD() {
+    var colors = getAllColors();
 
-var win = new Window("palette", "test2");
+    // 创建文件夹
+    CFG.folderName++;
+    var fileFolder = Folder.desktop + "/" + CFG.folderName + "/";
+    if (!Folder(fileFolder).exists) {
+        Folder(fileFolder).create();
+    }
 
-dimensionPanel = win.add("group", [0, 0, 290, 90]);
+    // 遍历所有颜色
+    for (var i = 0; i < colors.length; i++) {
+        var colorStr = colors[i];
 
-win.show();
+        // 获取当前文档
+        var doc = app.activeDocument;
+
+        function processItem(item) {
+            if (item.typename === "GroupItem") {
+                for (var i = 0; i < item.pageItems.length; i++) {
+                    processItem(item.pageItems[i]);
+                }
+            } else if (item.typename === "CompoundPathItem") {
+                // 处理复合路径中的每个路径项
+                for (var i = 0; i < item.pathItems.length; i++) {
+                    var pathItem = item.pathItems[i];
+                    if (pathItem.filled && pathItem.fillColor) {
+                        var itemColorStr = getColorString(pathItem.fillColor);
+                        pathItem.hidden = itemColorStr !== colorStr;
+                    }
+                }
+            } else if (item.filled && item.fillColor) {
+                var itemColorStr = getColorString(item.fillColor);
+                item.hidden = itemColorStr !== colorStr;
+            }
+        }
+
+        function getColorString(color) {
+            if (color.typename === "RGBColor") {
+                return "RGB(" + color.red + ", " + color.green + ", " + color.blue + ")";
+            } else if (color.typename === "CMYKColor") {
+                return "CMYK(" + color.cyan + ", " + color.magenta + ", " + color.yellow + ", " + color.black + ")";
+            } else if (color.typename === "GrayColor") {
+                return "Gray(" + color.gray + ")";
+            } else if (color.typename === "SpotColor") {
+                return "SpotColor(" + color.spot.name + ")";
+            }
+            return "";
+        }
+
+        // 遍历所有图层
+        for (var j = 0; j < doc.layers.length; j++) {
+            var layer = doc.layers[j];
+            if (!layer.locked && layer.visible) {
+                for (var k = 0; k < layer.pageItems.length; k++) {
+                    processItem(layer.pageItems[k]);
+                }
+            }
+        }
+
+        // 处理文件名
+        var fileName = colorStr.replace(/^SpotColor\(PANTONE (.+)\)/, "$1");
+        fileName = fileName.replace(/^RGB\((.+)\)/, "$1");
+        fileName = fileName.replace(/^CMYK\((.+)\)/, "$1");
+        fileName = fileName.replace(/^Gray\((.+)\)/, "$1");
+        fileName = fileName.replace(/[,]/g, "_");
+
+        exportToPSD(fileFolder + fileName + ".psd");
+
+        // 恢复所有项的显示状态
+        app.undo();
+        app.redraw();
+    }
+}
